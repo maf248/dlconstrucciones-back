748c039c06622d785b8bb114c138b76db2ba02a8
maf248 <maf_248@hotmail.com>
2021-10-14 19:41:51 -0300

    Terminé la poblar la base de datos, creando dos usuarios de prueba. Ya estan terminados el modelo de mySQL, el script de estructura y de poblacion, funcionan correctamente.

--- db/dln-db-population.sql
+++ db/dln-db-population.sql
@@ -109,21 +109,19 @@ INSERT INTO categories (title, image) VALUES
 ('Costa Esmeralda','costa-esmeralda.jfif'),
 ('Campo', 'campo.png');
 
-INSERT INTO batches (categories_id, title, description, image) VALUES
-(1,'Ecuestre - Lote 229','USD 28500 
-
-Lote en el exclusivo balneario Costa Esmeralda, ubicado a 10km de Pinamar (Ruta 11, km 380).
-El Lote es el Nro 229 del Barrio Ecuestre; el mismo se encuentra ubicado justo frente a la Cancha Nro. 1 de Polo del barrio con una ubicación privilegiada para observar los partidos.','lote-en-costa-esmeralda-barrio-ecuestre-D_NQ_NP_310915-MLA25335796838_022017-F.jpg' ),
-(1,'Marítimo III - Lote 117','El lote más alto en MARITIMO 3 FRENTE AL MAR !!!
+INSERT INTO batches (categories_id, title, price, sold, description, image) VALUES
+(1,'Ecuestre - Lote 229',28500,'false','Lote en el exclusivo balneario Costa Esmeralda, ubicado a 10km de Pinamar (Ruta 11, km 380).
+El Lote es el Nro 229 del Barrio Ecuestre; el mismo se encuentra ubicado justo frente a la Cancha Nro. 1 de Polo del barrio con una ubicación privilegiada para observar los partidos.','lote-en-costa-esmeralda-barrio-ecuestre-D_NQ_NP_310915-MLA25335796838_022017-F.jpg'),
+(1,'Marítimo III - Lote 117',NULL,'false','El lote más alto en MARITIMO 3 FRENTE AL MAR !!!
 Ubicado al lado de los paradores con una altura asegurada de 18 mts sobre el nivel del mar + (más) tu casa a construir de hasta 8 mts adicionales a esta altura. Por lo tanto puedes estar a 26 mts de altura. TODOS LOTES BAJOS entre el lote y el mar. Planimetría que demuestra la altura sobre el nivel del mar.
 Solo 1 vecino por lo tanto tu lote hacia uno de los laterales lo tenes libre y "amplia" tu parque.
 Actualmente están todos los papeles el dia y solo restan 7 cuotas de 800 USD cada una aproximados.
 EL PRECIO ES DE USD 105.000 mas las cuotas restantes.
 Cualquier lote bajo SIN VISTA AL MAR en los "BARRIOS MARÍTIMOS" VALEN:
 MARITIMO 1 entre USD 100.000 y usd 150.000
-MARITIMO 2 entre USD 150,000 y USD 250.000','el-lote-mas-alto-en-maritimo-3-vista-al-mar-asegurada--D_NQ_NP_749665-MLA29469965697_022019-F.jpg' ),
+MARITIMO 2 entre USD 150,000 y USD 250.000','el-lote-mas-alto-en-maritimo-3-vista-al-mar-asegurada--D_NQ_NP_749665-MLA29469965697_022019-F.jpg'),
 
-(1,'Casa en Golf II - Lote 313','### PRECIO REBAJADO ### PRECIO REBAJADO ### PRECIO REBAJADO ###
+(1,'Casa en Golf II - Lote 313',NULL,'false','### PRECIO REBAJADO ### PRECIO REBAJADO ### PRECIO REBAJADO ###
 ANTES USD 529.000
 AL COSTO !!! (Terreno + Construccion + Pileta +Quincho + Fogon + Pérgolas) AL COSTO !!!
 ACEPTO AUTOS y DEPARTAMENTOS DE 1 y 2 AMBIENTES !!!! DUEÑO DIRECTO !!!!!!!!!
@@ -169,22 +167,16 @@ RENTA ASEGURADA !!! La casa se alquiló este verano en :
 Diciembre 15 dias: $130.000
 Enero 31 días: $330.000
 Febrero 28 días: $230.000
-Marzo 10 días: $75.000','casa-exclusiva-isla-del-golf-precio-rebajado--D_NQ_NP_724766-MLA28228810210_092018-F.jpg' ),
-(1,'Senderos IV - Lote 349','USD 15000
-
-Espectacular lote en el Barrio Senderos 4 (Lote 349) de Costa Esmeralda - 980 m2','lote-costa-esmeralda-D_NQ_NP_949029-MLA27350389559_052018-F.jpg'),
-(1,'Senderos III - Lote 314','USD 45000
-
-Excelente terreno, super forestado!!
+Marzo 10 días: $75.000','casa-exclusiva-isla-del-golf-precio-rebajado--D_NQ_NP_724766-MLA28228810210_092018-F.jpg'),
+(1,'Senderos IV - Lote 349',15000,'false','Espectacular lote en el Barrio Senderos 4 (Lote 349) de Costa Esmeralda - 980 m2','lote-costa-esmeralda-D_NQ_NP_949029-MLA27350389559_052018-F.jpg'),
+(1,'Senderos III - Lote 314',45000,'false','Excelente terreno, super forestado!!
 Tiene 20 mtrs de frente por 50 mtrs de fondo y además atrás hay mas de 70 metros que no se pueden construir lo que le da una vista espectacular porque parece mas del doble de grande el terreno.
 Hay una casa al lado muy linda ya construida.
 Esta en el Sector Sendero 3 pero justo pegado a donde termina el sector Bosques por eso la increíble forestación que tiene.
 Esta absolutamente al día tantos con los impuestos municipales como con las expensas.
 Tiene los planos de mensura ya aprobados.
 Asi que esta listo para construir.','impresionante-terreno-costa-esmeralda-muy-forestado-D_NQ_NP_980316-MLA29499444911_022019-F.jpg' ),
-(1,'Bosque - Lote 9','USD 25000
-
-LOTE 9 DE BARRIO BOSQUE DE COSTA ESMERALDA.
+(1,'Bosque - Lote 9',25000,'false','LOTE 9 DE BARRIO BOSQUE DE COSTA ESMERALDA.
 BARRIO BOSQUE, es uno de los 14 barrios de COSTA ESMERALDA, FORMADO CON 50 EXCLUSIVOS LOTES. (mientras que otros barrios tienen en promedio 400 lotes). Otra diferencia con respecto a los lotes de otros barrios por lo que lo hace tan exclusivo, es que son LOTES DE 1300 M2 (los demás como máximo 1000 m2) Y QUE NO TIENE VECINOS CONTIGUOS ( mientras que los demás están pegados tanto de atrás como de los lados, ver adjunto del barrio). EL LOTE 9 (NUNCA VA TENER VECINOS ATRÁS) TIENE LA AGRIMENSURA YA REALIZADA (ver adjunto, los círculos son pinos). COMO SE PUEDE VER EN LAS FOTOS CUENTA CON ALTA DENSIDAD DE CONIFERAS MUY AÑOSAS.
 USD 29.000 CONTADO
 USD 32.000 CON FACILIDADES, 30% ANTICIPO Y 12 CUOTAS FIJAS EN DOLARES, CON POSESION INMEDIATA.
@@ -200,10 +192,8 @@ Características:
 - Deportes: Tenis (8 canchas hard court iluminadas y 4 canchas de tenis de polvo de ladrillo). Cancha de volley. Canchas de fútbol. Gimnasio. Ping pong. Pool. Actividades para los más chicos. Club house.
 - Dos canchas de polo, palenques, picadero, corrales, materas y pista de vareo. Cabalgatas diurnas y nocturnas todo el año.
 - Laguna de 2 has.
-LUZ, AGUA Y GAS NATURAL.','978752-MLA30047115568_042019-O.jpg' ),
-(1,'Deportiva - Lote 29','USD 31500
-
-Excelente lote de 1120 m2 en Cul de Sac. Sin vecinos ni lotes en la parte trasera, ya que da al boulevard que comunica con el barrio Ecuestre.
+LUZ, AGUA Y GAS NATURAL.','978752-MLA30047115568_042019-O.jpg'),
+(1,'Deportiva - Lote 29',31500,'false','Excelente lote de 1120 m2 en Cul de Sac. Sin vecinos ni lotes en la parte trasera, ya que da al boulevard que comunica con el barrio Ecuestre.
 
 A dos cuadras del Club House Deportivo: Gym y Canchas de Futbol, Tenis, Paddle... Muy linda zona, ya edificada en un 80%.
 
@@ -211,41 +201,29 @@ Lote ya amojonado por agrimensor. Impuestos y expensas al día.
 
 ¡Oportunidad! Valuado por dos inmobiliarias en 40 y 45000 dólares.
 
-Podría tomar auto o moto en parte de pago.','terreno-lote-en-costa-esmeralda-b-deportivo-l29-D_NQ_NP_677058-MLA26324233982_112017-F.jpg' ),
-(1,'Golf II - Lote 439','USD 50000
-
-Lote 439 Barrio Golf II COSTA ESMERALDA. El lote, cuenta con una superficie de 1056 mts2, ubicado en esquina
+Podría tomar auto o moto en parte de pago.','terreno-lote-en-costa-esmeralda-b-deportivo-l29-D_NQ_NP_677058-MLA26324233982_112017-F.jpg'),
+(1,'Golf II - Lote 439',50000,'false','Lote 439 Barrio Golf II COSTA ESMERALDA. El lote, cuenta con una superficie de 1056 mts2, ubicado en esquina
 Excelente potencial como inversión y desarrollo para calidad de vida
-Un emprendimiento integral y de avanzada, de descanso junto al mar ubicado sobre la Ruta 11, a 5 km de Pinamar por la playa y a 11 por la ruta. Es un masterplan de mil hectáreas y 3400 lotes, entre distintos barrios y áreas residenciales, que cuenta además con 3500 metros de costa de playa y plantaciones de pinos de diversas edades. Como infraestructura diferencial, presenta dos canchas de golf de 27 hoyos c/u, zonas deportivas y turísticas, una zona hípica con 2 canchas de polo y caballerizas. Se prevee el desarrollo de una zona comercial de alto impacto.','barrio-golf-ii-lote-439-D_NQ_NP_608413-MLA30412764299_052019-F.jpg' ),
-(1,'Ecuestre - Lote 137','USD 24000
-
-Terreno en Costa Esmeralda, Barrio Ecuestre, Lote 137, de rápido acceso, en esquina , con buenas cotas de altura, 974 m2 de superficie total listo para construir su proyecto.','ecuestre-137-excelente-terreno-dueno-vende-D_NQ_NP_874753-MLA28245981003_092018-F.jpg' ),
-(1,'Ecuestre - Lote 46','USD 38500
-
-¡OPORTUNIDAD!
+Un emprendimiento integral y de avanzada, de descanso junto al mar ubicado sobre la Ruta 11, a 5 km de Pinamar por la playa y a 11 por la ruta. Es un masterplan de mil hectáreas y 3400 lotes, entre distintos barrios y áreas residenciales, que cuenta además con 3500 metros de costa de playa y plantaciones de pinos de diversas edades. Como infraestructura diferencial, presenta dos canchas de golf de 27 hoyos c/u, zonas deportivas y turísticas, una zona hípica con 2 canchas de polo y caballerizas. Se prevee el desarrollo de una zona comercial de alto impacto.','barrio-golf-ii-lote-439-D_NQ_NP_608413-MLA30412764299_052019-F.jpg'),
+(1,'Ecuestre - Lote 137',24000,'false','Terreno en Costa Esmeralda, Barrio Ecuestre, Lote 137, de rápido acceso, en esquina , con buenas cotas de altura, 974 m2 de superficie total listo para construir su proyecto.','ecuestre-137-excelente-terreno-dueno-vende-D_NQ_NP_874753-MLA28245981003_092018-F.jpg'),
+(1,'Ecuestre - Lote 46',38500,'false','¡OPORTUNIDAD!
 Costa Esmeralda. Barrio Ecuestre. Lote 46. 895 m2
 Inmejorable zona, con fondo a reserva de poca vegetación y gran tamaño, acompañado de un lote con poca irregularidad en alturas genera amplias vistas.
 
-PRECIO DE CONTADO. ACEPTO 50% ANTICIPO Y 12 CUOTAS!!','costa-esmerala-barrio-privado-pinamar-lote-country-costa-D_NQ_NP_697495-MLA29617591047_032019-F.jpg' ),
-(1,'Residencial I - Lote 25','USD 45000
-
-Terreno en Barrio Residencial 1, Lote Nro.25. 1000 m2.
+PRECIO DE CONTADO. ACEPTO 50% ANTICIPO Y 12 CUOTAS!!','costa-esmerala-barrio-privado-pinamar-lote-country-costa-D_NQ_NP_697495-MLA29617591047_032019-F.jpg'),
+(1,'Residencial I - Lote 25',45000,'false','Terreno en Barrio Residencial 1, Lote Nro.25. 1000 m2.
 
 Uno de los pocos q quedan (el valor promedio de los restantes es de USD 60000). Ya está desmalezado y con agrimensura hecha. Listo para construir ¡Sin deudas!
-Escucho ofertas solo de contado. NO SE PERMUTA.','vendo-terreno-y-lote-costa-esmeralda-x-viaje-al-exterior-D_NQ_NP_800755-MLA28734561518_112018-F.jpg' ),
-(1,'Residencial I - Lote 121','USD 55000
-
-Excelente Lote en Barrio Residencial I de Costa Esmeralda. 960 m2
+Escucho ofertas solo de contado. NO SE PERMUTA.','vendo-terreno-y-lote-costa-esmeralda-x-viaje-al-exterior-D_NQ_NP_800755-MLA28734561518_112018-F.jpg'),
+(1,'Residencial I - Lote 121',55000,'false','Excelente Lote en Barrio Residencial I de Costa Esmeralda. 960 m2
 
 Muy cercano al Parador Norte, frente a espacio verde no edificable.
 
-Acepto que se tome en parte de pago por departamento de 2/3 ambientes en C.A.B.A., o zona sur de GBA (Sudeste).','costa-esmeralda-residencial-i-lote-121-D_NQ_NP_917316-MLA29794639598_032019-F.jpg' ),
-(1,'Senderos IV - Lote 218','USD 28500
+Acepto que se tome en parte de pago por departamento de 2/3 ambientes en C.A.B.A., o zona sur de GBA (Sudeste).','costa-esmeralda-residencial-i-lote-121-D_NQ_NP_917316-MLA29794639598_032019-F.jpg'),
+(1,'Senderos IV - Lote 218',28500,'false','Barrio Senderos 4 Costa Esmeralda - Lote 218 - 906 mts2 con fondo a pulmón. No perimetral','terreno-venta-lote-D_NQ_NP_248615-MLA25291293011_012017-F.jpg'),
+(1,'Lote en Senderos II',NULL,'true','Excepcional lote en Senderos II
 
-Barrio Senderos 4 Costa Esmeralda - Lote 218 - 906 mts2 con fondo a pulmón. No perimetral','terreno-venta-lote-D_NQ_NP_248615-MLA25291293011_012017-F.jpg' ),
-(1,'Lote en Senderos II','Excepcional lote en Senderos II
-
-1042 m2','Lote vendido 1.jpg' );
+1042 m2','Lote vendido 1.jpg');
 
 INSERT INTO images (batches_id, image) VALUES
 (3,'casa-exclusiva-isla-del-golf-precio-rebajado-D-NQ-NP-656997-MLA28228825228-092018-F.jpg'),
@@ -346,8 +324,6 @@ INSERT INTO images (batches_id, image) VALUES
 (13, 'venta-lote-terreno-D-NQ-NP-473615-MLA25291290120-012017-F.jpg');
 
 
--- INSERT INTO users (hash_id, first_name, last_name, email, password, image, role, product_id, category_id) VALUES 
--- ('$2a$10$1EHxHf5rQ.gyJ2xnJ55sFeHVnMlxbXReGoaFC1ID/dhumC8mGG8Ii','Tobias','Blaksley','tobias.blaksley@hotmail.com','$2a$10$nY9qIx.MJ4cXVG5csGpFFeQzQl4J2h0zhT.VndbdlnZOGzYjdbcmW','Usuario-1_1610562779647.jfif','admin',1,1),
--- ('$2a$10$qpbUssE9j0VlJYCAlUeDweZLwvsKpHYOM9aJ7TjAAGyBLH.5xmmbu','Marcelo','Fanego','maf_248@hotmail.com','$2a$10$/hWEc7y3EoCeSEhnVgkMPOKQJvccgJUlx5xTwMbQquUgEQwEvPw0y','Usuario-2_1611071885248.png','admin',2,1),
--- ('$2a$10$sXa4gEy5HjWWl/BokcMd1e0wgNAXzl3sfyRmGLfa.PfFqhmN5jBpO','Sergio','Casas','sergio.casasr@utadeo.edu.co','$2a$10$A4iqY8lnKfLXCcH38AM/MeOcVkDzG1mxcRf/VllprSbUlFYNwvcg2','Usuario-3_1611147303044.jpeg','admin',2,1),
--- ('$2a$10$y4PFWqy9lA0agnmPmeOrOOgTt6M8f2m1461QT4edtwWMmz3ZHnfte','Marcelo','Fanego','mafmaggot@gmail.com','$2a$10$MHA8vyzxjf4h8rg.WVSPEeXvh9ZX5Uv/a9s58zz70WrnOQdkNLMhW','Usuario-4_1611071905072.png','user',NULL,NULL); 
+INSERT INTO users (first_name, last_name, email, password, avatar, dni, role) VALUES 
+('Tobias','Blaksley','tobias.blaksley@hotmail.com','$2a$10$nY9qIx.MJ4cXVG5csGpFFeQzQl4J2h0zhT.VndbdlnZOGzYjdbcmW','Usuario-1_1610562779647.jfif',NULL,'admin'),
+('Marcelo','Fanego','mafmaggot@gmail.com','$2a$10$MHA8vyzxjf4h8rg.WVSPEeXvh9ZX5Uv/a9s58zz70WrnOQdkNLMhW','Usuario-4_1611071905072.png',35960622,'user');
