92151ad604e76a5d78155da2ed4199ebd7a303e1
maf248 <maf_248@hotmail.com>
2021-10-14 18:45:11 -0300

    Cree y completé casi totalmente el archivo de poblacion de la base de datos sql, en base a los assets de la página original y lo acordado.

---
+++ db/population.sql
@@ -0,0 +1,254 @@
+INSERT INTO interests (title, description, image) VALUES
+('Publica tu lote en venta en nuestra página', 'El éxito de nuestra empresa se basa en el "boca a boca" y es común que nuestros clientes, luego de haber utilizado nuestros servicios, nos refieran a sus amigos. Nuestro negocio es la construcción, pero en numerosas oportunidades nos han solicitado ayuda y asesoramiento para la adquisición de terrenos.
+
+Es por eso que recientemente hemos agregado a nuestros servicios la sección "Lotes en Venta". Te ofrecemos la posibilidad de mostrar, de primera mano, tu lote en la costa a potenciales interesados.','Lote vendido 2.png'),
+('Domótica: Confort, seguridad, comunicación y ahorro en el hogar','La influencia de la era digital en nuestra forma de vida y los cambios sociales han hecho de la tecnología domótica una herramienta indispensable en nuestros hogares al permitir instalar sistemas de automatización de viviendas que garantizan confort, seguridad, facilitan las comunicaciones, aportan accesibilidad a cada uno de los espacios y contribuye al ahorro.
+
+La domótica ofrece diseños automatizados e inteligentes que son accesibles por la mayoría de consumidores, convierte su hogar en un lugar confortable, más personal, multifuncional, flexible y seguro para toda la familia.
+
+En el mercado existen múltiples propuestas de soluciones eléctricas adecuadas a cada vivienda particular.
+
+En los últimos años, se ha popularizado el uso de controles a distancia para programar el encendido y apagado de calentadores, aires acondicionados, televisores, equipos musicales, toldos y persianas eléctricas, automatizar y regular la iluminación, abrir o cerrar puertas y ventanas de manera remota.
+
+De igual forma, estos controles modernos permiten activar alarmas de intrusión, detectar humo, gas o escapes de agua y gestionar de manera eficiente el consumo eléctrico con el uso de iluminación LED, lo cual nos permite ahorrar energía e iluminar sus ambientes de manera bonita, agradable y confortable.
+
+DLN Construcciones ofrece estos servicios gracias a sus expertos, especializados en la instalación de todo tipo de sistemas eléctricos de baja tensión y en la gestión hogares seguros y cómodos a través de la instalación de alarmas automáticas, sistemas contra incendios, porteros automáticos, videoporteros y cámaras de videovigilancia. Asimismo, instalamos dispositivos de entretenimiento para toda la familia como antenas parabólicas, TV Satélite y Televisión Digital Terrestre, y también realizamos proyectos de iluminación artística y decorativa para piscinas y jardines.
+
+En definitiva, en el momento de necesitar el mejor servicio, DLN Construcciones tiene todas las respuestas para su hogar.','domotica.jpg'),
+('Hogares sustentables', 'Termotanques solares:
+
+$$ Ahorro de GAS
+Uso de energía alternativa renovable
+Esta tecnología permite un ahorro de hasta un 60% en el consumo de gas en termotanque/calefones y sistemas de radiadores/losa radiante al elevar la temperatura inicial del agua.
+
+Doble y Triple acristalamiento:
+
+$$ Ahorro de GAS
+$$ Ahorro de ELECTRICIDAD
+Combinada con aberturas de PVC o de Aluminio con RPT (Rotura de Puente Térmico) producen una aislación térmica y acústica, evitando así la pérdida de energía calórica de su hogar y evitando la polución sonora exterior.
+
+Ventilación Mecánica Controlada (VMC):
+
+$$ Ahorro de Electricidad
+Mejora el bienestar de su familia
+El sistema de VMC permite la extracción del aire viciado de su casa y su renovación por aire exterior, sin la consiguiente pérdida de temperatura (frío/calor) y pasando por una serie de filtros, evitando así la proliferación de hongos y bacterias en su hogar.
+
+Recaptación y reciclaje de aguas pluviales y de consumo:
+
+Cuidado del medio ambiente
+El uso racional del agua es una de las mejores maneras de cuidar el medio ambiente. La reutilización de un recurso limitado como es el agua en tareas como el riego de su jardín es una de las estrategias verdaderamente eco-sustentables de un hogar moderno.','termo-solar.jpg');
+
+INSERT INTO services (title, description, image) VALUES
+('Vivienda','Adquisición de lotes
+
+Te asesoramos en la compra del terreno que mejor se adapte a tu proyecto, tomando en cuenta presupuesto, ubicación, nivel del lote y aquellos detalles que pueden pasar inadvertidos para el ojo inexperto.
+
+Proyecto de obra
+
+Estamos con vos en cada etapa, desde la idea original hasta el día en que ingreses por primera vez a tu casa terminada. Podemos guiarte en el proceso de diseño y desarrollo de un espacio que se adapte a tus características familiares y a tu estilo de vida.
+
+Ejecucion
+
+Contamos con una completa gama de profesionales de la construcción, bien establecidos y de amplia experiencia. Nos ocupamos de todos los detalles, desde la construcción más tradicional hasta desarrollos eco-sustentables y tecnología domótica.
+
+Direccion de obra
+
+Desde el primer día vas a estar al tanto de los adelantos y el control de tu obra, con "updates"  semanales a través de tu e-mail o de Whatsapp.
+
+Llave en mano
+
+Limpieza, terminaciones, mobiliario... todo lo que necesites para poder disfrutar de tu nuevo hogar desde el día del estreno. DLN Construcciones es la solución a tu medida. No lo dudes más, contactanos.','timthumb1.png'),
+('Antenas de Telecomunicaciones','Antenas de Telecomunicaciones
+Ejecución de Obra Civil en todo el país
+Montaje de Estructuras
+P.A.T.
+Ensayos de Hormigón Armado','timthumb2.png'),
+('Piscinas',NULL,'timthumb3.png');
+
+INSERT INTO pictures (services_id, picture) VALUES
+(3,'Vista-03.jpg'),
+(3,'Vista-05.jpg'),
+(3,'WhatsApp-Image-2019-03-23-at-11.43.31.jpeg'),
+(3,'WhatsApp-Image-2019-03-23-at-11.43.32.jpeg'),
+(3,'WhatsApp-Image-2019-03-23-at-11.43.49.jpeg'),
+(3,'WhatsApp-Image-2019-03-23-at-11.43.50.jpeg'),
+(3, 'WhatsApp-Image-2019-03-23-at-11.43.58.jpeg');
+
+INSERT INTO types (title, image) VALUES
+('Construcción','construccion.jfif'),
+('Antenas de Telecomunicaciones','antena.jfif');
+
+INSERT INTO jobs (types_id, image) VALUES
+(1,'WhatsApp-Image-2019-01-31-at-20.13.05.jpeg'),
+(1,'WhatsApp-Image-2019-01-31-at-20.13.06.jpeg'),
+(1,'WhatsApp-Image-2019-01-31-at-20.13.17-1-.jpeg'),
+(1,'WhatsApp-Image-2019-01-31-at-20.13.19.jpeg'),
+(1,'WhatsApp-Image-2019-01-31-at-20.13.22-2-.jpeg'),
+(1,'WhatsApp-Image-2019-01-31-at-20.18.49.jpeg'),
+(1,'WhatsApp-Image-2019-01-31-at-20.19.00.jpeg'),
+(1,'WhatsApp-Image-2019-01-31-at-20.19.01-1-.jpeg'),
+
+(2,'WhatsApp-Image-2019-01-31-at-20.27.58.jpeg'),
+(2,'WhatsApp-Image-2019-01-31-at-20.28.57.jpeg'),
+(2,'WhatsApp-Image-2019-01-31-at-20.29.28.jpeg'),
+(2,'WhatsApp-Image-2019-01-31-at-20.29.35.jpeg'),
+(2,'WhatsApp-Image-2019-01-31-at-20.29.42.jpeg'),
+(2,'WhatsApp-Image-2019-01-31-at-20.29.43.jpeg'),
+(2,'WhatsApp-Image-2019-01-31-at-20.29.44.jpeg'),
+(2,'WhatsApp-Image-2019-01-31-at-20.29.44-1-.jpeg'),
+(2,'WhatsApp-Image-2019-01-31-at-20.29.45.jpeg'),
+(2,'WhatsApp-Image-2019-01-31-at-20.29.47-1-.jpeg'),
+(2,'WhatsApp-Image-2019-01-31-at-20.29.48.jpeg'),
+(2,'WhatsApp-Image-2019-01-31-at-20.29.49.jpeg'),
+(2,'WhatsApp-Image-2019-01-31-at-20.29.50.jpeg'),
+(2,'WhatsApp-Image-2019-01-31-at-20.29.51.jpeg'),
+(2,'WhatsApp-Image-2019-01-31-at-20.29.52.jpeg'),
+(2,'WhatsApp-Image-2019-01-31-at-20.29.52-2-.jpeg');
+
+INSERT INTO categories (title, image) VALUES
+('Costa Esmeralda','costa-esmeralda.jfif'),
+('Campo', 'campo.png');
+
+INSERT INTO batches (categories_id, title, description, image) VALUES
+(1,'Ecuestre - Lote 229','USD 28500 
+
+Lote en el exclusivo balneario Costa Esmeralda, ubicado a 10km de Pinamar (Ruta 11, km 380).
+El Lote es el Nro 229 del Barrio Ecuestre; el mismo se encuentra ubicado justo frente a la Cancha Nro. 1 de Polo del barrio con una ubicación privilegiada para observar los partidos.','lote-en-costa-esmeralda-barrio-ecuestre-D_NQ_NP_310915-MLA25335796838_022017-F.jpg' ),
+(1,'Marítimo III - Lote 117','El lote más alto en MARITIMO 3 FRENTE AL MAR !!!
+Ubicado al lado de los paradores con una altura asegurada de 18 mts sobre el nivel del mar + (más) tu casa a construir de hasta 8 mts adicionales a esta altura. Por lo tanto puedes estar a 26 mts de altura. TODOS LOTES BAJOS entre el lote y el mar. Planimetría que demuestra la altura sobre el nivel del mar.
+Solo 1 vecino por lo tanto tu lote hacia uno de los laterales lo tenes libre y "amplia" tu parque.
+Actualmente están todos los papeles el dia y solo restan 7 cuotas de 800 USD cada una aproximados.
+EL PRECIO ES DE USD 105.000 mas las cuotas restantes.
+Cualquier lote bajo SIN VISTA AL MAR en los "BARRIOS MARÍTIMOS" VALEN:
+MARITIMO 1 entre USD 100.000 y usd 150.000
+MARITIMO 2 entre USD 150,000 y USD 250.000','el-lote-mas-alto-en-maritimo-3-vista-al-mar-asegurada--D_NQ_NP_749665-MLA29469965697_022019-F.jpg' ),
+
+(1,'Casa en Golf II - Lote 313','### PRECIO REBAJADO ### PRECIO REBAJADO ### PRECIO REBAJADO ###
+ANTES USD 529.000
+AL COSTO !!! (Terreno + Construccion + Pileta +Quincho + Fogon + Pérgolas) AL COSTO !!!
+ACEPTO AUTOS y DEPARTAMENTOS DE 1 y 2 AMBIENTES !!!! DUEÑO DIRECTO !!!!!!!!!
+RENTA ASEGURADA !!! La casa se alquiló este verano en :
+Diciembre 15 dias: $130.000
+Enero 31 días: $330.000
+Febrero 28 días: $230.000
+Marzo 10 días: $75.000
+
+CASA EXCLUSIVA en la ISLA del GOLF!!!
+EXCELENTE y MODERNO DISEÑO (mismo estudio del complejo AL GOLF 19)
+Vista al GREEN DEL HOYO 7 (Hoyo 25)
+PLANTA ALTA interior
+COMEDOR con mesa para 12 personas
+LIVING COMEDOR con mesa tipo FOGON
+COCINA y TOILETTE
+PLANTA ALTA exterior
+GALERÍA con PARRILLA y mesa para 12 personas
+LIVING EXTERIOR CON FOGON para 10 personas
+BALCÓN CON VISTA AL PARQUE, PILETA, FOGÓN y HOYO 7
+PLANTA BAJA interior
+1 DORMITORIO EN SUITE
+2 DORMITORIOS EN SEMISUITE
+1 PLAYROOM CON 4 CAMAS MARINERAS
+1 DEPENDENCIA CON BAÑO
+LAVADERO
+PLANTA BAJA exterior
+CUARTO HERRAMIENTAS
+TENDER
+QUINCHO para 12 personas
+PILETA DE 7 mts X 3,5 mts CLIMATIZADA !!!
+FOGÓN exclusivo para 25 personas
+VARIOS:
+Loza radiante en toda la casa
+5 Aires acondicionados Frío/Calor
+Totalmente amueblada
+
+### PRECIO REBAJADO ### PRECIO REBAJADO ### PRECIO REBAJADO ###
+ANTES USD 529.000
+AL COSTO !!! (Terreno + Construccion + Pileta +Quincho + Fogon + Pérgolas) AL COSTO !!!
+ACEPTO AUTOS y DEPARTAMENTOS DE 1 y 2 AMBIENTES !!!! DUEÑO DIRECTO !!!!!!!!!
+RENTA ASEGURADA !!! La casa se alquiló este verano en :
+Diciembre 15 dias: $130.000
+Enero 31 días: $330.000
+Febrero 28 días: $230.000
+Marzo 10 días: $75.000','casa-exclusiva-isla-del-golf-precio-rebajado--D_NQ_NP_724766-MLA28228810210_092018-F.jpg' ),
+(1,'Senderos IV - Lote 349','USD 15000
+
+Espectacular lote en el Barrio Senderos 4 (Lote 349) de Costa Esmeralda - 980 m2','lote-costa-esmeralda-D_NQ_NP_949029-MLA27350389559_052018-F.jpg'),
+(1,'Senderos III - Lote 314','USD 45000
+
+Excelente terreno, super forestado!!
+Tiene 20 mtrs de frente por 50 mtrs de fondo y además atrás hay mas de 70 metros que no se pueden construir lo que le da una vista espectacular porque parece mas del doble de grande el terreno.
+Hay una casa al lado muy linda ya construida.
+Esta en el Sector Sendero 3 pero justo pegado a donde termina el sector Bosques por eso la increíble forestación que tiene.
+Esta absolutamente al día tantos con los impuestos municipales como con las expensas.
+Tiene los planos de mensura ya aprobados.
+Asi que esta listo para construir.','impresionante-terreno-costa-esmeralda-muy-forestado-D_NQ_NP_980316-MLA29499444911_022019-F.jpg' ),
+(1,'Bosque - Lote 9','USD 25000
+
+LOTE 9 DE BARRIO BOSQUE DE COSTA ESMERALDA.
+BARRIO BOSQUE, es uno de los 14 barrios de COSTA ESMERALDA, FORMADO CON 50 EXCLUSIVOS LOTES. (mientras que otros barrios tienen en promedio 400 lotes). Otra diferencia con respecto a los lotes de otros barrios por lo que lo hace tan exclusivo, es que son LOTES DE 1300 M2 (los demás como máximo 1000 m2) Y QUE NO TIENE VECINOS CONTIGUOS ( mientras que los demás están pegados tanto de atrás como de los lados, ver adjunto del barrio). EL LOTE 9 (NUNCA VA TENER VECINOS ATRÁS) TIENE LA AGRIMENSURA YA REALIZADA (ver adjunto, los círculos son pinos). COMO SE PUEDE VER EN LAS FOTOS CUENTA CON ALTA DENSIDAD DE CONIFERAS MUY AÑOSAS.
+USD 29.000 CONTADO
+USD 32.000 CON FACILIDADES, 30% ANTICIPO Y 12 CUOTAS FIJAS EN DOLARES, CON POSESION INMEDIATA.
+COSTA ESMERALDA
+Barrio privado a sólo 10´de Pinamar, que con sus 1.000 hectáreas y 3.200 m de costa conforman un lugar exclusivo para familias o parejas con ganas de apostar su proyecto a un entorno natural de pinos y mar, comodidad, relax, deporte y buena vida. En el área residencial se encuentran más de 3900 lotes para viviendas unifamiliares, algunos aptos para construir y otros en desarrollo.
+Características:
+- Sobre Ruta 11, km 38, del Partido de la Costa. A solo 10´de Pinamar.
+- Superficie de 1.000 hectáreas con plantaciones de pinos y 3.200 metros de costa exclusivos.
+- Servicios: seguridad 24hs., proveeduría abierta todo el año y una iglesia en el acceso al barrio.
+- Compuesto por siete sectores con características propias: Bosque, Deportiva, Ecuestre, Golf, Senderos, Residencial, Marítimo.
+- Dos paradores: Cabo Norte (para disfrutar la playa en familia) y Cabo Suelto (con deportes acuáticos).
+- Cancha de golf de 18 hoyos y de 9 hoyos. Driving range y putting green. Club house.
+- Deportes: Tenis (8 canchas hard court iluminadas y 4 canchas de tenis de polvo de ladrillo). Cancha de volley. Canchas de fútbol. Gimnasio. Ping pong. Pool. Actividades para los más chicos. Club house.
+- Dos canchas de polo, palenques, picadero, corrales, materas y pista de vareo. Cabalgatas diurnas y nocturnas todo el año.
+- Laguna de 2 has.
+LUZ, AGUA Y GAS NATURAL.','978752-MLA30047115568_042019-O.jpg' ),
+(1,'Deportiva - Lote 29','USD 31500
+
+Excelente lote de 1120 m2 en Cul de Sac. Sin vecinos ni lotes en la parte trasera, ya que da al boulevard que comunica con el barrio Ecuestre.
+
+A dos cuadras del Club House Deportivo: Gym y Canchas de Futbol, Tenis, Paddle... Muy linda zona, ya edificada en un 80%.
+
+Lote ya amojonado por agrimensor. Impuestos y expensas al día.
+
+¡Oportunidad! Valuado por dos inmobiliarias en 40 y 45000 dólares.
+
+Podría tomar auto o moto en parte de pago.','terreno-lote-en-costa-esmeralda-b-deportivo-l29-D_NQ_NP_677058-MLA26324233982_112017-F.jpg' ),
+(1,'Golf II - Lote 439','USD 50000
+
+Lote 439 Barrio Golf II COSTA ESMERALDA. El lote, cuenta con una superficie de 1056 mts2, ubicado en esquina
+Excelente potencial como inversión y desarrollo para calidad de vida
+Un emprendimiento integral y de avanzada, de descanso junto al mar ubicado sobre la Ruta 11, a 5 km de Pinamar por la playa y a 11 por la ruta. Es un masterplan de mil hectáreas y 3400 lotes, entre distintos barrios y áreas residenciales, que cuenta además con 3500 metros de costa de playa y plantaciones de pinos de diversas edades. Como infraestructura diferencial, presenta dos canchas de golf de 27 hoyos c/u, zonas deportivas y turísticas, una zona hípica con 2 canchas de polo y caballerizas. Se prevee el desarrollo de una zona comercial de alto impacto.','barrio-golf-ii-lote-439-D_NQ_NP_608413-MLA30412764299_052019-F.jpg' ),
+(1,'Ecuestre - Lote 137','USD 24000
+
+Terreno en Costa Esmeralda, Barrio Ecuestre, Lote 137, de rápido acceso, en esquina , con buenas cotas de altura, 974 m2 de superficie total listo para construir su proyecto.','ecuestre-137-excelente-terreno-dueno-vende-D_NQ_NP_874753-MLA28245981003_092018-F.jpg' ),
+(1,'Ecuestre - Lote 46','USD 38500
+
+¡OPORTUNIDAD!
+Costa Esmeralda. Barrio Ecuestre. Lote 46. 895 m2
+Inmejorable zona, con fondo a reserva de poca vegetación y gran tamaño, acompañado de un lote con poca irregularidad en alturas genera amplias vistas.
+
+PRECIO DE CONTADO. ACEPTO 50% ANTICIPO Y 12 CUOTAS!!','costa-esmerala-barrio-privado-pinamar-lote-country-costa-D_NQ_NP_697495-MLA29617591047_032019-F.jpg' ),
+(1,'Residencial I - Lote 25','USD 45000
+
+Terreno en Barrio Residencial 1, Lote Nro.25. 1000 m2.
+
+Uno de los pocos q quedan (el valor promedio de los restantes es de USD 60000). Ya está desmalezado y con agrimensura hecha. Listo para construir ¡Sin deudas!
+Escucho ofertas solo de contado. NO SE PERMUTA.','vendo-terreno-y-lote-costa-esmeralda-x-viaje-al-exterior-D_NQ_NP_800755-MLA28734561518_112018-F.jpg' ),
+(1,'Residencial I - Lote 121','USD 55000
+
+Excelente Lote en Barrio Residencial I de Costa Esmeralda. 960 m2
+
+Muy cercano al Parador Norte, frente a espacio verde no edificable.
+
+Acepto que se tome en parte de pago por departamento de 2/3 ambientes en C.A.B.A., o zona sur de GBA (Sudeste).','costa-esmeralda-residencial-i-lote-121-D_NQ_NP_917316-MLA29794639598_032019-F.jpg' ),
+(1,'Senderos IV - Lote 218','USD 28500
+
+Barrio Senderos 4 Costa Esmeralda - Lote 218 - 906 mts2 con fondo a pulmón. No perimetral','terreno-venta-lote-D_NQ_NP_248615-MLA25291293011_012017-F.jpg' ),
+(1,'Lote en Senderos II','Excepcional lote en Senderos II
+
+1042 m2','Lote vendido 1.jpg' );
+
+-- INSERT INTO users (hash_id, first_name, last_name, email, password, image, role, product_id, category_id) VALUES 
+-- ('$2a$10$1EHxHf5rQ.gyJ2xnJ55sFeHVnMlxbXReGoaFC1ID/dhumC8mGG8Ii','Tobias','Blaksley','tobias.blaksley@hotmail.com','$2a$10$nY9qIx.MJ4cXVG5csGpFFeQzQl4J2h0zhT.VndbdlnZOGzYjdbcmW','Usuario-1_1610562779647.jfif','admin',1,1),
+-- ('$2a$10$qpbUssE9j0VlJYCAlUeDweZLwvsKpHYOM9aJ7TjAAGyBLH.5xmmbu','Marcelo','Fanego','maf_248@hotmail.com','$2a$10$/hWEc7y3EoCeSEhnVgkMPOKQJvccgJUlx5xTwMbQquUgEQwEvPw0y','Usuario-2_1611071885248.png','admin',2,1),
+-- ('$2a$10$sXa4gEy5HjWWl/BokcMd1e0wgNAXzl3sfyRmGLfa.PfFqhmN5jBpO','Sergio','Casas','sergio.casasr@utadeo.edu.co','$2a$10$A4iqY8lnKfLXCcH38AM/MeOcVkDzG1mxcRf/VllprSbUlFYNwvcg2','Usuario-3_1611147303044.jpeg','admin',2,1),
+-- ('$2a$10$y4PFWqy9lA0agnmPmeOrOOgTt6M8f2m1461QT4edtwWMmz3ZHnfte','Marcelo','Fanego','mafmaggot@gmail.com','$2a$10$MHA8vyzxjf4h8rg.WVSPEeXvh9ZX5Uv/a9s58zz70WrnOQdkNLMhW','Usuario-4_1611071905072.png','user',NULL,NULL); 
