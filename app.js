013ac876c8cb9a43fc99fa9abcfa3d4dc8f3087e
maf248 <maf_248@hotmail.com>
2021-12-01 19:23:23 -0300

    Modificacion de la estructura del repositorio de backend.

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const cors = require('cors');
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+
+const app = express();
+const bodyParser = require("body-parser");
+const port = 3000;
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(cors());
+app.use(express.urlencoded({
+  extended: false
+}));
+app.use(cookieParser());
+
+app.use(bodyParser.json());
+app.use(express.static(process.cwd() + "/public/"));
+
+app.get('/', (req, res) => {
+  res.sendFile(process.cwd() + "/public/index.html")
+});
+
+app.use('/api', indexRouter);
+app.use('/api/users', usersRouter);
+
+module.exports = app;
--- server/README.md
+++
@@ -1,1 +0,0 @@
-# dlconstrucciones-back
