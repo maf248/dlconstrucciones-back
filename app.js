1d4e9fadb98ccde0ed10de9c8788d345f1a19299
maf248 <maf_248@hotmail.com>
2021-12-03 21:14:33 -0300

    Agregue gitignore para node modules

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const cors = require('cors');
-const bodyParser = require("body-parser");
+const bodyParser = require('body-parser');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
@@ -19,11 +19,16 @@ app.use(express.urlencoded({
 app.use(cookieParser());
 
 app.use(bodyParser.json());
-app.use(express.static(process.cwd() + "/public/"));
+app.use(express.static(process.cwd() + '/public/'));
 
 app.use('/api', indexRouter);
 app.use('/api/users', usersRouter);
 
-
+app.get('/', (req, res) => {
+  res.sendFile(process.cwd() + '/public/index.html')
+});
+app.get('*', (req, res) => {
+  res.sendFile(process.cwd() + '/public/index.html')
+});
 
 module.exports = app;
