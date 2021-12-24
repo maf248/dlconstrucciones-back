f7d86fc5b483da46f34099f34d2da2a1103d7d95
maf248 <maf_248@hotmail.com>
2021-12-24 12:24:10 -0300

    Complete el CRUD de categorias de lotes, con validacion backend y solo accesible para usuarios admin.

--- routes/categories.js
+++ routes/categories.js
@@ -1,11 +1,38 @@
 var express = require('express');
 var router = express.Router();
+const path = require('path');
+const multer = require('multer');
 
 // Importing API controllers
 const categoriesController = require('../controllers/categoriesController');
 
+// Importing middleware for protected routes
+const adminWebTokenMiddleware = require('../middlewares/adminWebTokenMiddleware');
+
+// Importing backend validations
+const categoryValidate = require('../middlewares/validation/categoryValidate');
+
+// Multer
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        let dirImage = path.join('public', 'images')
+        return cb(null, dirImage);
+    },
+    filename: function (req, file, cb) {
+
+        return cb(null, 'Category' + '_' + Date.now() + path.extname(file.originalname));
+    },
+});
+
+const upload = multer({
+    storage: storage
+});
+
 // Categories Routes
 router.get('/', categoriesController.index);
 router.get('/:id', categoriesController.detail);
+router.post('/create', adminWebTokenMiddleware, upload.single('image'), categoryValidate, categoriesController.create);
+router.patch('/edit/:id', adminWebTokenMiddleware, upload.single('image'), categoryValidate, categoriesController.edit);
+router.delete('/delete/:id', adminWebTokenMiddleware, categoriesController.delete);
 
 module.exports = router;
