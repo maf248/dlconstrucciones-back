e0510dc8e0f7bd7d0719ec5f80a24fd634cbf33a
maf248 <maf_248@hotmail.com>
2022-03-22 21:05:33 -0300

    Se agrega middleware headers cors fix.

--- app.js
+++ app.js
@@ -31,6 +31,14 @@ app.use(
     extended: false,
   })
 );
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
+});
+
 app.use(cookieParser());
 
 app.use(bodyParser.json());
