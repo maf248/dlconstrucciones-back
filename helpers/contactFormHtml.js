36667341f4922850d62491b7fcac91fcad7d0802
maf248 <maf_248@hotmail.com>
2022-03-25 20:57:30 -0300

    Correción url en mail helpers con url nueva.

--- helpers/contactFormHtml.js
+++ helpers/contactFormHtml.js
@@ -26,7 +26,7 @@ contactFormHtml = (email, comment) => {
             <table role="presentation" style="width:602px;border-collapse:collapse;border:1px solid #cccccc;border-spacing:0;text-align:left;">
               <tr>
                 <td align="center" style="padding:40px 0 30px 0;background:radial-gradient(white, #4bb0d0);">
-                  <img src="https://lnconstrucciones.herokuapp.com/assets/logo.png" alt="DLN Construcciones" width="300" style="height:auto;display:block;" />
+                  <img src="https://dlnconstrucciones.net/assets/logo.png" alt="DLN Construcciones" width="300" style="height:auto;display:block;" />
                 </td>
               </tr>
               <tr>
@@ -53,7 +53,7 @@ contactFormHtml = (email, comment) => {
                   <table role="presentation" style="width:100%;border-collapse:collapse;border:0;border-spacing:0;font-size:9px;font-family:Arial,sans-serif;">
                     <tr>
                       <td style="padding:0;width:50%;" align="left">
-                        <p style="margin:0;font-size:14px;line-height:16px;font-family:Arial,sans-serif;color:#ffffff;"><a href="https://lnconstrucciones.herokuapp.com/" style="color:#ffffff;text-decoration:none;">
+                        <p style="margin:0;font-size:14px;line-height:16px;font-family:Arial,sans-serif;color:#ffffff;"><a href="https://dlnconstrucciones.net/" style="color:#ffffff;text-decoration:none;">
                           @ 2022 DLN Construcciones<br/></a>
                         </p>
                       </td>
@@ -61,10 +61,10 @@ contactFormHtml = (email, comment) => {
                         <table role="presentation" style="border-collapse:collapse;border:0;border-spacing:0;">
                           <tr>
                             <td style="padding:0 0 0 20px;width:38px;">
-                              <a href="https://www.instagram.com/dln_construcciones/" style="color:#ffffff;"><img src="https://lnconstrucciones.herokuapp.com/assets/icon-instagram.png" alt="Twitter" width="38" style="height:auto;display:block;border:0;" /></a>
+                              <a href="https://www.instagram.com/dln_construcciones/" style="color:#ffffff;"><img src="https://dlnconstrucciones.net/assets/icon-instagram.png" alt="Twitter" width="38" style="height:auto;display:block;border:0;" /></a>
                             </td>
                             <td style="padding:0 0 0 20px;width:38px;">
-                              <a href="https://www.facebook.com/dlnconstrucciones/" style="color:#ffffff;"><img src="https://lnconstrucciones.herokuapp.com/assets/icon-facebook.png" alt="Facebook" width="38" style="height:auto;display:block;border:0;" /></a>
+                              <a href="https://www.facebook.com/dlnconstrucciones/" style="color:#ffffff;"><img src="https://dlnconstrucciones.net/assets/icon-facebook.png" alt="Facebook" width="38" style="height:auto;display:block;border:0;" /></a>
                             </td>
                           </tr>
                         </table>
