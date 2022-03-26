3f5570f31d036eeddf42ceb07fb1d986a599ebdf
maf248 <maf_248@hotmail.com>
2022-03-26 13:20:27 -0300

    Agrego secure false

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -14,7 +14,7 @@ module.exports = {
         var transporter = nodemailer.createTransport({
           host: process.env.NODEMAILER_HOST,
           port: process.env.NODEMAILER_PORT,
-          secure: true,
+          secure: false,
           auth: {
             user: process.env.NODEMAILER_USER,
             pass: process.env.NODEMAILER_PASS,
