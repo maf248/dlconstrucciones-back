9080f0684215776e825a17333da0060001c026c9
maf248 <maf_248@hotmail.com>
2022-03-26 12:49:37 -0300

    Secure false para nodemailer.

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,10 +1,8 @@
-const {
-  validationResult
-} = require("express-validator");
+const { validationResult } = require("express-validator");
 const nodemailer = require("nodemailer");
 const contactFormHtml = require("../helpers/contactFormHtml");
 const constactClientHtml = require("../helpers/contactClientHtml");
-const fs = require('fs')
+const fs = require("fs");
 
 module.exports = {
   contact: (req, res, next) => {
@@ -16,7 +14,7 @@ module.exports = {
         var transporter = nodemailer.createTransport({
           host: process.env.NODEMAILER_HOST,
           port: process.env.NODEMAILER_PORT,
-          secure: true,
+          secure: false,
           auth: {
             user: process.env.NODEMAILER_USER,
             pass: process.env.NODEMAILER_PASS,
@@ -33,41 +31,53 @@ module.exports = {
         transporter.sendMail(mailContactFormOptions, function (error, info) {
           if (error) {
             console.log(error);
-            fs.appendFileSync('../errors-log.txt', ", \n" + JSON.stringify({
-              mensaje: error,
-              lugar: `transporter.sendmail - contactController.js - nodemailer`
-            }))
+            fs.appendFileSync(
+              "../errors-log.txt",
+              ", \n" +
+                JSON.stringify({
+                  mensaje: error,
+                  lugar: `transporter.sendmail - contactController.js - nodemailer`,
+                })
+            );
           } else {
             console.log("Email sent: " + info.response);
+            fs.appendFileSync(
+              "../errors-log.txt",
+              ", \n" +
+                JSON.stringify({
+                  mensaje: "ok",
+                  lugar: `transporter.sendmail - contactController.js - nodemailer`,
+                })
+            );
           }
         });
 
-        var mailClientFormOptions = {
-          from: `${process.env.NODEMAILER_USER}`,
-          to: `${process.env.NODEMAILER_USER}`,
-          subject: "Recibiste una consulta del sitio web - DLN Construcciones",
-          html: constactClientHtml(req.body.email, req.body.comment),
-        };
+        //   var mailClientFormOptions = {
+        //     from: `${process.env.NODEMAILER_USER}`,
+        //     to: `${process.env.NODEMAILER_USER}`,
+        //     subject: "Recibiste una consulta del sitio web - DLN Construcciones",
+        //     html: constactClientHtml(req.body.email, req.body.comment),
+        //   };
 
-        transporter.sendMail(mailClientFormOptions, function (error, info) {
-          if (error) {
-            console.log(error);
-            fs.appendFileSync('../errors-log.txt', ", \n" + JSON.stringify({
-              mensaje: error,
-              lugar: `transporter.sendmail - contactController.js - nodemailer`
-            }))
-          } else {
-            console.log("Email sent: " + info.response);
-            fs.appendFileSync('../errors-log.txt', ", \n" + JSON.stringify({
-              mensaje: JSON.stringify(info),
-              lugar: `transporter.sendmail - contactController.js - nodemailer`
-            }))
-          }
-        });
+        //   transporter.sendMail(mailClientFormOptions, function (error, info) {
+        //     if (error) {
+        //       console.log(error);
+        //       fs.appendFileSync('../errors-log.txt', ", \n" + JSON.stringify({
+        //         mensaje: error,
+        //         lugar: `transporter.sendmail - contactController.js - nodemailer`
+        //       }))
+        //     } else {
+        //       console.log("Email sent: " + info.response);
+        //       fs.appendFileSync('../errors-log.txt', ", \n" + JSON.stringify({
+        //         mensaje: JSON.stringify(info),
+        //         lugar: `transporter.sendmail - contactController.js - nodemailer`
+        //       }))
+        //     }
+        //   });
       }
 
       main()
-        .then(
+        .then(() => {
           res.json({
             meta: {
               status: 200,
@@ -77,14 +87,27 @@ module.exports = {
               comment: req.body.comment,
               message: "Emails Sent Correctly",
             },
-          })
-        )
-        .catch(err => {
+          });
+
+          fs.appendFileSync(
+            "../errors-log.txt",
+            ", \n" +
+              JSON.stringify({
+                mensaje: "ok",
+                lugar: `transporter.sendmail - contactController.js - nodemailer`,
+              })
+          );
+        })
+        .catch((err) => {
           console.error(err);
-          fs.appendFileSync('../errors-log.txt', ", \n" + JSON.stringify({
-            mensaje: err,
-            lugar: `catch - nodemailer - contactController.js`
-          }))
+          fs.appendFileSync(
+            "../errors-log.txt",
+            ", \n" +
+              JSON.stringify({
+                mensaje: err,
+                lugar: `catch - nodemailer - contactController.js`,
+              })
+          );
         });
     } else {
       return res.json({
