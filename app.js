067cbf36be5cd7256816e1778a743491183f3b70
maf248 <maf_248@hotmail.com>
2022-03-26 17:13:04 -0300

    Quito middleware innecesario de app.js.

--- app.js
+++ app.js
@@ -31,13 +31,6 @@ app.use(
     extended: false,
   })
 );
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-  res.setHeader('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
-  next();
-});
 
 app.use(cookieParser());
 
