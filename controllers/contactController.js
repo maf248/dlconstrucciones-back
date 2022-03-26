1703bc7cc40d7bd33c51de4419b44d24a62529ca
maf248 <maf_248@hotmail.com>
2022-03-26 13:10:33 -0300

    Se agrega configuracion nodemailer debbuging.ç

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -14,11 +14,14 @@ module.exports = {
         var transporter = nodemailer.createTransport({
           host: process.env.NODEMAILER_HOST,
           port: process.env.NODEMAILER_PORT,
-          secure: false,
+          secure: true,
           auth: {
             user: process.env.NODEMAILER_USER,
             pass: process.env.NODEMAILER_PASS,
           },
+          tls: {
+            rejectUnauthorized: false
+          }
         });
 
         var mailContactFormOptions = {
